62ec887518a3fcea53ea1741bb075c471ac0cc26
purguinhatb <106619423+purguinhatb@users.noreply.github.com>
2024-06-20 11:38:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
@@ -13,6 +13,7 @@ function diminuiTamanho(){
         tamanhoSenha--;
 }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
@@ -20,6 +21,28 @@ function aumentaTamanho(){
         tamanhoSenha++;
 }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-console.log(botoes);
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+console.log(checkbox[0].checked);
+
+const letrasMaiúsculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const letrasMinúsculas = 'abcdefghijklmnopqrstuvwxyz'
+const números = '0123456789';
+const símbolos = '!@%*?';
+
+geraSenha();
+
+function geraSenha(){
+    let alfabeto = ''
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiúsculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiúsculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
